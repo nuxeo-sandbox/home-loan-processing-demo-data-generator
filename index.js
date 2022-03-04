4b752ad9af51f81730d1497005e5f8c106164237
michael <mvachette@nuxeo.com>
2022-03-04 11:30:36 +0900

    Upgrade faker version

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const faker = require('faker');
-const moment = require('moment');
-const fastcsv = require('fast-csv');
-const fs = require('fs');
-const commandLineArgs = require('command-line-args');
+import { faker } from '@faker-js/faker';
+import moment  from 'moment';
+import  fastcsv  from 'fast-csv';
+import  fs  from 'fs';
+import commandLineArgs from 'command-line-args';
 
 const optionDefinitions = [
   { name: 'locale', type: String, defaultValue: "en_US" },
   { name: 'number', type: Number, defaultValue: 100  },
-  { name: 'output', type: String, defaultValue: "output.csv"  },
+  { name: 'output', type: String, defaultValue: "metadata.csv"  },
 ]
 
 const options = commandLineArgs(optionDefinitions);
@@ -23,6 +23,7 @@ for (let i = start; i < end; i++) {
   records.push({
     "type": "Application",
     "name": "AP"+i,
+    "ecm:currentLifeCycleState": faker.random.arrayElement(["new","assigned","canceled","approved","declined"]),
     "dc:created": faker.date.past(0.3).toISOString(),
     "dc:creator": "Administrator",
     "applicant:firstname": faker.name.firstName(),
@@ -39,7 +40,7 @@ for (let i = start; i < end; i++) {
     "applicant:phone_number": faker.phone.phoneNumber(),
     "applicant:occupation": faker.random.arrayElement(["employee","public_servant","executive","freelancer","other"]),
     "applicant:employment_type": faker.random.arrayElement(["permanent","contract","other"]),
-    "applicant:employment_duration": faker.random.number({min:1,max:40,precision:1}),
+    "applicant:employment_duration": faker.datatype.number({min:1,max:40,precision:1}),
     "applicant:employer_name": faker.company.companyName(),
     "applicant:employer_address": JSON.stringify({
       postalcode: faker.address.zipCode(),
@@ -47,7 +48,7 @@ for (let i = start; i < end; i++) {
       prefecture: faker.address.state(),
       line1: faker.address.streetName()
     }),
-    "propertyd:additional_costs": faker.random.number({min:1000000,max:1000000000,precision:100}),
+    "propertyd:additional_costs": faker.datatype.number({min:1000000,max:1000000000,precision:100}),
     "propertyd:address": JSON.stringify({
       postalcode: faker.address.zipCode(),
       city: faker.address.city(),
@@ -55,15 +56,15 @@ for (let i = start; i < end; i++) {
       line1: faker.address.streetName()
     }),
     "propertyd:property_type": faker.random.arrayElement(["apartment","detached_house"]),
-    "propertyd:purchase_price": faker.random.number({min:1000000,max:1000000000,precision:100}),
-    "propertyd:own_funds": faker.random.number({min:1000000,max:1000000000,precision:100}),
+    "propertyd:purchase_price": faker.datatype.number({min:1000000,max:1000000000,precision:100}),
+    "propertyd:own_funds": faker.datatype.number({min:1000000,max:1000000000,precision:100}),
     "propertyd:purchase_date": faker.date.future().toISOString(),
-    "loan:amount":faker.random.number({min:1000000,max:1000000000,precision:100}),
-    "loan:term":faker.random.number({min:10,max:40,precision:1}),
+    "loan:amount":faker.datatype.number({min:1000000,max:1000000000,precision:100}),
+    "loan:term":faker.datatype.number({min:10,max:40,precision:1}),
     "loan:borrowing_date": faker.date.future().toISOString(),
     "loan:interest_type": faker.random.arrayElement(["floating","fixed"]),
     "loan:purpose": faker.random.arrayElement(["purchase_new","purchase_used","refinance","construct"]),
-    "loan:selling_and_buying": faker.random.boolean(),
+    "loan:selling_and_buying": faker.datatype.boolean(),
     "application:demo":true,
     "application:format": faker.random.arrayElement(["digital","paper"]),
     "application:type": faker.random.arrayElement(["preapproval","regular"])
